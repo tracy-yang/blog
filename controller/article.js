38fa081a78243eae9907d4f8dcd18b4a1fac7848
yangxy <yangxy@juziwl.com>
2018-05-18 18:20:15 +0800

    新增了模板页面

--- controller/article.js
+++ controller/article.js
@@ -2,36 +2,54 @@ let articles = require('../models/articles');
 let util = require('../util/baseRes')
 
 class Article {
+    // 接口
     getNewsList(req,res){
-        let page = Number(req.body.page) || 10;
-        let row = Number(req.body.row) || 1;
-        let start = (page - 1)*row;
-        let list = null;
+            let page = Number(req.body.page) || 10;
+            let row = Number(req.body.row) || 1;
+            let start = (row - 1)*page;
+            let list = null;
+            let info = articles.find({}).limit(page).skip(start);
+            // 方法1：
+            let result = new Promise((resolve,reject) =>{
+                info.exec((err,data) =>{
+                    console.log(data);
+                    if(err) throw err;
+                    resolve(data);
+                })
+            })
+            result.then(data =>{
+                console.log(data);
+                // res.send(util.setResult(200,'查询列表成功',data,util.pagination(page,row)));
+            })
+
+            // 方法2 报错不成功
+            // let a = info.exec((err,doc) =>{
+            //     if(err) throw err;
+            // })
+            // a.then(data =>{
+            //     console.log(data);
+            // })       
+    }
+
+    // 给ejs传递新闻列表
+    getNews(page=10,row=1){
+        let list = [];
+        let start = (row - 1)*page;
         let info = articles.find({}).limit(page).skip(start);
-        // 方法1：
         let result = new Promise((resolve,reject) =>{
             info.exec((err,data) =>{
                 if(err) throw err;
                 resolve(data);
             })
         })
-        result.then(data =>{
-            res.send(util.setResult(200,'查询列表成功',data,util.pagination(page,row)));
-        })
-
-        // 方法2 报错不成功
-        // let a = info.exec((err,doc) =>{
-        //     if(err) throw err;
-        // })
-        // a.then(data =>{
-        //     console.log(data);
-        // })        
+        return result;
     }
 
+    // 新增新闻接口
     addNews(req,res){
         let info = new articles({
-            title:'这是第二篇文章',
-            content:'这是第二篇文章的内容发的咖啡机的卡了几分链接发的卡拉解放立刻搭街坊了大家发了打算减肥的',
+            title:'这是第四篇文章',
+            content:'这是第四篇文章的内容发的咖啡机的卡了几分链接发的卡拉解放立刻搭街坊了大家发了打算减肥的',
             createUser:'test01',
             state:'1',
             createTime:'2018-5-17 18:00:00'
@@ -41,6 +59,16 @@ class Article {
             res.send(util.setResult(200,'插入成功'));
         })
     }
+
+    // 根据ID查询新闻详情
+    getNewsDetail(id){
+        articles.findById(id,(err,data)=>{
+            console.log(data);
+        })
+    }
+
+
+
 
 }
 
--- routes/article.js
+++ routes/article.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 let Article = require('../controller/article')
+let articles = require('../models/articles');
+let util = require('../util/baseRes')
+let mongoose = require('mongoose');
 
 router.get('/',(req,res,next) => {
-    res.render('article',{});
+    Article.getNews().then(data => {
+        res.render('article',{'data':data});
+    }); 
 })
 router.post('/getNewsList',Article.getNewsList);
 router.post('/addNews',Article.addNews);
+router.get('/detail/:id',(req,res,next) =>{
+    let oid = mongoose.Type.ObjectId(req.params);
+    console.log(oid)
+    articles.findById(oid,(err,data)=>{
+        console.log('111111',data);
+    })
+    res.render('articleDetail');
+})
 
 module.exports = router;
